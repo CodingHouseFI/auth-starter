8cf2bde65b1e9e2cd666d29162a8d63253e8acc7
Cade Nichols <cade@cadenichols.com>
2016-01-30 16:47:10 -0800

    login with expanded model

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost/herokutest'
 var mongoose = require('mongoose');
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost/moremongo';
 mongoose.connect(mongoUrl, function(err) {
-  console.log(err || `Connected to MongoDB: ${mongoUrl}`);
+  if(err) {
+    console.log('Mongo error:', err);
+  } else {
+    console.log(`MongoDB connected to ${mongoUrl}`);
+  }
 });
 
 // view engine setup
@@ -18,16 +22,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-app.use('/pokemon', require('./routes/pokemon'));
+app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var authMiddleware = require('../config/auth');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Cade's App" });
-});
-
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
-
-router.get('/register', function(req, res, next) {
-  res.render('register');
-});
-
-router.get('/secret', authMiddleware, function(req, res, next) {
-  console.log('req.user:', req.user);
-  res.send('Wooo!  Secret stuff!!!');
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
